26b9c814731c7a89de34b295c9bbc2fa5fee39b3
yozakura1379 <72366000+yozakura1379@users.noreply.github.com>
2020-10-16 16:29:20 +0900

    既読機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,20 @@
 class PostsController < ApplicationController
-  def index  # indexアクションを定義した
+  def index
     @posts = Post.all.order(id: "DESC")
   end
-
   def create
     Post.create(content: params[:content])
     redirect_to action: :index
   end
-end
+  def checked
+    post = Post.find(params[:id])
+    if post.checked
+      post.update(checked: false) 
+    else
+      post.update(checked: true)
+    end
+    item = Post.find(params[:id]) #これはid取得した時、取得した情報は全てjson形式で返すという意味、これがないとjavaのファイルが実行されない
+    render json: { post: item }  #renderは返すという意味、postにitemを代入したようなもの、実際はハッシュの属性と値の関係
+  end
 
+end
--- db/migrate/20200925065746_create_posts.rb
+++ db/migrate/20200925065746_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
       t.text :content
-      t.boolean :checked
+      t.boolean :checked #このbooleanという型はtrueか fulesを返す、これの動作をクリックにすればクリックしたら動作が働くようになる
       t.timestamps
     end
   end
